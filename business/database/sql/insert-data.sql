7b00d8f8f6cac5037e0ee9cdb85de4d2811f031f
Kevin Guanche Darias <kevin@kevinguanchedarias.com>
2018-07-12 11:24:53 +0100

    (Bug) Unit Missions are not working, they are getting stuck

--- business/database/sql/insert-data.sql
+++ business/database/sql/insert-data.sql
@@ -25,7 +25,7 @@ INSERT INTO mission_types (id,code,description,is_shared) VALUES (1,'LEVEL_UP','
 INSERT INTO mission_types (id,code,description,is_shared) VALUES (2,'BROADCAST_MESSAGE','Envía un mensaje a todos los usuarios conectados',0);
 INSERT INTO mission_types (id,code,description,is_shared) VALUES (3,'BUILD_UNIT','Construye una unidad',0);
 INSERT INTO mission_types (id,code,description,is_shared) VALUES (4,'EXPLORE','Explores a planet',0);
-INSERT INTO mission_types (id,code,description,is_shared) VALUES (5,'RETURN_MISSIONT','Returns the unit to the source planet',0);
+INSERT INTO mission_types (id,code,description,is_shared) VALUES (5,'RETURN_MISSION','Returns the unit to the source planet',0);
 INSERT INTO mission_types (id,code,description,is_shared) VALUES (6,'GATHER','Gather resources from specified planet',0);
 INSERT INTO mission_types (id,code,description,is_shared) VALUES (7,'ESTABLISH_BASE','Establish a base in a new planet',0);
 INSERT INTO mission_types (id,code,description,is_shared) VALUES (8,'ATTACK','Attacks an user',0);
