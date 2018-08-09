940b107732650751481fac20080303215035495e
Kevin Guanche Darias <kevin@kevinguanchedarias.com>
2018-08-09 11:31:26 +0100

    (Task) Create endpoint to return unit types and their limit (if any), with base, and the total available and the consumed
    Fix, not sending image

--- business/src/main/java/com/kevinguanchedarias/sgtjava/dto/UnitTypeDto.java
+++ business/src/main/java/com/kevinguanchedarias/sgtjava/dto/UnitTypeDto.java
@@ -7,6 +7,7 @@ public class UnitTypeDto implements DtoFromEntity<UnitType> {
 
 	private Integer id;
 	private String name;
+	private String image;
 	private Long maxCount;
 	private Long computedMaxCount;
 	private Long userBuilt;
@@ -30,6 +31,14 @@ public class UnitTypeDto implements DtoFromEntity<UnitType> {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
 	}
 
 	public Long getMaxCount() {
