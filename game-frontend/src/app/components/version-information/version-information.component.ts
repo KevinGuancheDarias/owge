599db6545b3feea30d7035108b08304ff89a96db
Kevin Guanche Darias <kevin@kevinguanchedarias.com>
2018-09-07 23:06:09 +0100

    (Bug) Changelog is opening the external links in the same page

--- game-frontend/src/app/components/version-information/version-information.component.ts
+++ game-frontend/src/app/components/version-information/version-information.component.ts
@@ -16,24 +16,37 @@ export class VersionInformationComponent implements OnInit {
     this._http.get('assets/html/changelog.html', { responseType: 'text' }).subscribe(result => {
       this.htmlContent = result;
       setTimeout(() => {
-        Array.from(this._getElement().querySelectorAll('img')).forEach(current => {
-          const parent: HTMLElement = current.parentElement;
-          if (parent.tagName === 'LI') {
-            parent.style.cursor = 'pointer';
-            parent.onclick = () => {
-              if (current.style.display !== 'none') {
-                current.style.display = 'none';
-              } else {
-                current.style.display = 'block';
-              }
-            };
-          }
-        });
+        this._handleImages();
+        this._handleLinks();
       }, 500);
     });
   }
 
   private _getElement(): HTMLElement {
     return this._ref.nativeElement;
+  }
+
+  private _findElements<T extends HTMLElement = HTMLElement>(selector: string): T[] {
+    return Array.from<T>(this._getElement().querySelectorAll<any>(selector));
+  }
+
+  private _handleLinks(): void {
+    this._findElements<HTMLLinkElement>('a').forEach(current => current.target = '_blank');
+  }
+
+  private _handleImages(): void {
+    this._findElements('img').forEach(current => {
+      const parent: HTMLElement = current.parentElement;
+      if (parent.tagName === 'LI') {
+        parent.style.cursor = 'pointer';
+        parent.onclick = () => {
+          if (current.style.display !== 'none') {
+            current.style.display = 'none';
+          } else {
+            current.style.display = 'block';
+          }
+        };
+      }
+    });
   }
 }
