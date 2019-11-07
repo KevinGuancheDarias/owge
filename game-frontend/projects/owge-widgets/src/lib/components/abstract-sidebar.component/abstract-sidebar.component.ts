13370af42bdb8377212d5b9a41784eac78e3c78b
Kevin Guanche Darias <kevin@kevinguanchedarias.com>
2019-11-07 20:11:48 +0000

    Fix #116  regression, removal of logout button D:

--- game-frontend/projects/owge-widgets/src/lib/components/abstract-sidebar.component/abstract-sidebar.component.ts
+++ game-frontend/projects/owge-widgets/src/lib/components/abstract-sidebar.component/abstract-sidebar.component.ts
@@ -3,23 +3,27 @@ import { MenuRoute } from '@owge/core';
 
 export abstract class AbstractSidebarComponent {
 
-    public constructor(private _internalTranslateService: TranslateService) { }
+  public constructor(private _internalTranslateService: TranslateService) { }
 
-   /**
-   * Creates a translatable <i>MenuRoute</i> (Handles translation of the menu button)
-   *
-   * @since 0.8.0
-   * @author Kevin Guanche Darias <kevin@kevinguanchedarias.com>
-   * @param textIndex The name of the translation in the file
-   * @param path
-   * @param icon
-   */
-  protected _createTranslatableMenuRoute(textIndex: string, path: string, icon: string): MenuRoute {
-    const retVal: MenuRoute =  {
+  /**
+  * Creates a translatable <i>MenuRoute</i> (Handles translation of the menu button)
+  *
+  * @since 0.8.0
+  * @author Kevin Guanche Darias <kevin@kevinguanchedarias.com>
+  * @param textIndex The name of the translation in the file
+  * @param pathOrAction
+  * @param icon
+  */
+  protected _createTranslatableMenuRoute(textIndex: string, pathOrAction: string | Function, icon: string): MenuRoute {
+    const retVal: MenuRoute = {
       text: 'Loading...',
-      path,
       icon
     };
+    if (typeof pathOrAction === 'function') {
+      retVal.action = pathOrAction;
+    } else {
+      retVal.path = pathOrAction;
+    }
     this._internalTranslateService.get(textIndex).subscribe(value => retVal.text = value);
     return retVal;
   }
