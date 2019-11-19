a5fa05c9b9a0dd19fb9af5cd688ae5457170cd3b
Kevin Guanche Darias <kevin@kevinguanchedarias.com>
2019-11-19 04:36:36 +0000

    Advance to version 0.8.1
    Add script to make version changing easy

---
+++ game-frontend/scripts/set-project-version.js
@@ -0,0 +1,40 @@
+const JSDOM = require('jsdom').JSDOM;
+const replaceInFile = require('replace-in-file');
+
+const pomFiles = [
+    '../business/pom.xml',
+    '../game-rest/pom.xml'
+];
+const packageJsonFiles = [
+    './package.json',
+    './projects/owge-alliance/package.json',
+    './projects/owge-core/package.json',
+    './projects/owge-faction/package.json',
+    './projects/owge-galaxy/package.json',
+    './projects/owge-universe/package.json',
+    './projects/owge-widgets/package.json'
+];
+if (process.argv.length >= 3) {
+    const targetVersion = process.argv[2];
+    pomFiles.forEach(async pomFile => {
+        console.log(`Changing version to  file ${pomFile}`);
+        const dom = await JSDOM.fromFile(pomFile);
+        const versionEl = dom.window.document.querySelector('project > version');
+        replaceInFile({
+            files: pomFile,
+            from: `<version>${versionEl.innerHTML}</version>`,
+            to: `<version>${targetVersion}-SNAPSHOT</version>`
+        });
+    });
+    packageJsonFiles.forEach(async packageJson => {
+        console.log(`Changing version to  file ${packageJson}`);
+        const oldVersion = require(`.${packageJson}`).version;
+        replaceInFile({
+            files: packageJson,
+            from: `"version": "${oldVersion}"`,
+            to: `"version": "${targetVersion}"`
+        });
+    });
+} else {
+    console.error('Must specify version, string must not contain the "v" prefix');
+}
