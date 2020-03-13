8bf74d2ca71dab2df243f50ae9acea4cb6af71e1
Kevin Guanche Darias <kevin@kevinguanchedarias.com>
2020-03-13 11:50:25 +0000

    Task #213

--- docker-ci/dev/images_creation/backend_rest/build.sh
+++ docker-ci/dev/images_creation/backend_rest/build.sh
@@ -2,5 +2,5 @@
 source '../../../ci/lib.sh';
 
 docker build -t owge_game_rest \
-    --target game-rest \
+    --target game-rest-and-admin \
     -f ../Dockerfile_for_war_files ../../../..
--- docker-ci/dev/images_creation/backend_rest/run.sh
+++ docker-ci/dev/images_creation/backend_rest/run.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 source '../../../ci/lib.sh';
-envFailureCheck "OWGE_DATABASE_URL" "$OWGE_DATABASE_URL"
-envFailureCheck "OWGE_DATABASE_USER" "$OWGE_DATABASE_USER"
-envFailureCheck "OWGE_DATABASE_PASSWORD" "$OWGE_DATABASE_PASSWORD"
+envFailureCheck "OWGE_DB_URL" "$OWGE_DB_URL"
+envFailureCheck "OWGE_DB_USER" "$OWGE_DB_USER"
+envFailureCheck "OWGE_DB_PASS" "$OWGE_DB_PASS"
 
 defaultStaticDirectory="/var/owge_data/static";
 defaultDynamicDirectory="/var/owge_data/dynamic";
@@ -11,4 +12,7 @@ dynamicDirectory="${2:-/var/owge_data/static}";
 
 dockerImage="owge_game_rest";
 checkDockerImageExists "$dockerImage"
-docker run -d -p 8080:8080 -v "$staticDirectory:/var/owge_data/static" -v "$dynamicDirectory:/var/owge_data/dynamic" --env CATALINA_OPTS="-Dowge.databaseUrl=${OWGE_DATABASE_URL} -Dowge.databaseUser=${OWGE_DATABASE_USER} -Dowge.databasePassword=${OWGE_DATABASE_PASSWORD}" "$dockerImage"
+docker run -t -p 8080:8080 -v "$staticDirectory:/var/owge_data/static" -v "$dynamicDirectory:/var/owge_data/dynamic"\
+    --env OWGE_DB_URL="$OWGE_DB_URL" --env OWGE_DB_USER="$OWGE_DB_USER" --env OWGE_DB_PASS="$OWGE_DB_PASS"\
+    --env MYSQL_HOST="192.168.99.1" --env MYSQL_USER="$OWGE_DB_USER" --env MYSQL_PASSWORD="$OWGE_DB_PASS" --env MYSQL_DB="sgalactica_java"\
+     "$dockerImage"
