d721316cf205a4f0d7f437fe4463f49c435aadcc
Kevin Guanche Darias <kevin@kevinguanchedarias.com>
2020-06-23 14:39:39 +0100

    update dependencies and remove old projects

--- account/src/main/java/com/kevinguanchedarias/owgejava/exception/CommonException.java
+++
@@ -1,9 +0,0 @@
-package com.kevinguanchedarias.owgejava.exception;
-
-public class CommonException extends RuntimeException {
-	private static final long serialVersionUID = 2272784657160377463L;
-
-	public CommonException(String message) {
-		super(message);
-	}
-}
--- account/src/main/java/com/kevinguanchedarias/owgejava/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kevinguanchedarias.owgejava.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kevinguanchedarias.owgejava.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-	public User findByEmail(String email);
-}
