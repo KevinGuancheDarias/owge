fe015c2c1f0c107ac7717d98be69b49654b7584d
Kevin Guanche Darias <kevin@kevinguanchedarias.com>
2020-06-27 14:33:49 +0100

    Complete #16

--- game-frontend/projects/game-frontend/src/app/components/deployed-units-list/deployed-units-list.component.ts
+++ game-frontend/projects/game-frontend/src/app/components/deployed-units-list/deployed-units-list.component.ts
@@ -9,9 +9,9 @@ import { UnitTypeService } from '../../services/unit-type.service';
 @Component({
   selector: 'app-deployed-units-list',
   templateUrl: './deployed-units-list.component.html',
-  styleUrls: ['./deployed-units-list.component.less']
+  styleUrls: ['./deployed-units-list.component.less', './deployed-units-list.component.scss']
 })
-export class DeployedUnitsListComponent implements OnInit, OnChanges {
+export class DeployedUnitsListComponent implements OnChanges {
 
   @Input()
   public obtainedUnits: ObtainedUnit[];
@@ -47,31 +47,66 @@ export class DeployedUnitsListComponent implements OnInit, OnChanges {
   @Output()
   public unitTypesOfSelection: EventEmitter<UnitType[]> = new EventEmitter();
 
-  public selectedCounts: number[];
   public unitTypes: UnitType[] = [];
+  public parsedObtained: {
+    [key: number]: {
+      typeName: string,
+      obtainedUnits?: ObtainedUnit[],
+      selectedCounts: number[],
+      allSelected: boolean
+    }
+  } = {};
 
   constructor(private _unitTypeService: UnitTypeService) { }
 
-  public ngOnInit() {
-    this.ngOnChanges();
-  }
-
   public ngOnChanges() {
+    this.parsedObtained = {};
     if (this.obtainedUnits) {
-      this.selectedCounts = this.obtainedUnits.map(() => null);
+      this.obtainedUnits
+        .filter(obtainedUnit => obtainedUnit.unit.typeId)
+        .forEach(obtainedUnit => {
+          const typeIndex: number = obtainedUnit.unit.typeId;
+          if (this.parsedObtained[typeIndex]) {
+            this.parsedObtained[typeIndex].selectedCounts.push(null);
+            this.parsedObtained[typeIndex].obtainedUnits.push(obtainedUnit);
+          } else {
+            this.parsedObtained[typeIndex] = {
+              typeName: obtainedUnit.unit.typeName,
+              obtainedUnits: [obtainedUnit],
+              selectedCounts: [null],
+              allSelected: false
+            };
+          }
+        });
     }
   }
 
+
+  /**
+   *
+   *
+   * @author Kevin Guanche Darias <kevin@kevinguanchedarias.com>
+   * @since 0.9.0
+   */
+  public clickAllOfType(unitTypeId): void {
+    const currentAllSelected = this.parsedObtained[unitTypeId].allSelected;
+    this.parsedObtained[unitTypeId].obtainedUnits
+      .forEach((obtainedUnit, i) => this.parsedObtained[unitTypeId].selectedCounts[i] = currentAllSelected ? 0 : obtainedUnit.count);
+    this.parsedObtained[unitTypeId].allSelected = !currentAllSelected;
+  }
+
   public async selectionChanged(): Promise<void> {
-    this.selection.emit(
-      this.selectedCounts.map<SelectedUnit>((current, index) => {
-        return {
-          unit: this.obtainedUnits[index].unit,
-          count: current
-        };
-      }).filter(current => current.count)
-    );
-    const ids: number[] = this.selectedCounts.map<number>(
+    const selectedCounts: SelectedUnit[] = [];
+    Object.keys(this.parsedObtained).forEach(typeId => {
+      this.parsedObtained[+typeId].obtainedUnits.forEach((obtainedUnit, index) => {
+        selectedCounts.push({
+          unit: obtainedUnit.unit,
+          count: this.parsedObtained[+typeId].selectedCounts[index]
+        });
+      });
+    });
+    this.selection.emit(selectedCounts.filter(current => current.count));
+    const ids: number[] = selectedCounts.map<number>(
       (current, index) => current ? this.obtainedUnits[index].unit.typeId : null
     ).filter(current => current !== null);
     this.unitTypesOfSelection.emit(await this._unitTypeService.idsToUnitTypes(...ids));
