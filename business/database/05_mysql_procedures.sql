9b37d815eea31dbd7def5dd4ff2edddc37ff2cd2
Kevin Guanche Darias <kevin@kevinguanchedarias.com>
2020-08-28 06:48:09 +0100

    Fix missing translation for sync error reload button Added procedure to remove orphan object_relations

--- business/database/05_mysql_procedures.sql
+++ business/database/05_mysql_procedures.sql
@@ -1,3 +1,27 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`%` PROCEDURE `DELETE_RELATION`(IN `i_relation_id` INT UNSIGNED)
+    NO SQL
+BEGIN
+DELETE FROM requirements_information WHERE relation_id = i_relation_id;
+DELETE from unlocked_relation WHERE relation_id = i_relation_id;
+DELETE FROM `object_relations` WHERE `object_relations`.`id` = i_relation_id;
+COMMIT;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `FIND_BLOCKED_MISSIONS`()
+    NO SQL
+SELECT * FROM `missions` WHERE type >= 3 AND resolved=0 AND termination_date < DATE_SUB(NOW(), INTERVAL 121 MINUTE ) ORDER BY id DESC$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `FIND_ORPHAN_UNITS`()
+    NO SQL
+SELECT * FROM obtained_units ou 
+	LEFT JOIN missions m ON m.id = ou.mission_id
+    WHERE ou.mission_id IS NOT NULL AND m.id IS NULL$$
+DELIMITER ;
 
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `DELETE_BLOCKED_MISSIONS`()
@@ -16,18 +40,4 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `DELETE_ORPHAN_UNITS`()
 DELETE ou FROM obtained_units ou 
 	LEFT JOIN missions m ON m.id = ou.mission_id
     WHERE ou.mission_id IS NOT NULL AND m.id IS NULL$$
-DELIMITER ;
-
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `FIND_ORPHAN_UNITS`()
-    NO SQL
-SELECT * FROM obtained_units ou 
-	LEFT JOIN missions m ON m.id = ou.mission_id
-    WHERE ou.mission_id IS NOT NULL AND m.id IS NULL$$
-DELIMITER ;
-
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `FIND_BLOCKED_MISSIONS`()
-    NO SQL
-SELECT * FROM `missions` WHERE type >= 3 AND resolved=0 AND termination_date < DATE_SUB(NOW(), INTERVAL 121 MINUTE ) ORDER BY id DESC$$
 DELIMITER ;
