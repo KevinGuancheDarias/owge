7aa413d21b86a0a6300a0368a39c36e53646d17e
Kevin Guanche Darias <kevin@kevinguanchedarias.com>
2020-11-02 20:58:39 +0000

    Fix admin panel stopped working again

--- game-frontend/projects/game-admin/src/environments/environment.prod.ts
+++ game-frontend/projects/game-admin/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   accountUrl: 'https://worlds.kevinguanchedarias.com',
-  loginEndpoint: '/oauth/token',
+  loginEndpoint: 'oauth/token',
   loginDomain: 'owge.net',
   loginClientId: '1e39e154-8ec1-4c72-81ed-48b47a2a7dd2',
   loginClientSecret: '1234'
